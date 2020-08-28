8b7469a73d860d1ad3e2d5dcb614d8d427b4edef
CodeAnti <CodeAnti@zhuniu.com>
2020-08-28 16:04:59 +0800

    init project

--- googlesearch/spiders/searchSpider.py
+++ googlesearch/spiders/searchSpider.py
@@ -5,15 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from urllib.parse import urlparse, parse_qs, parse_qsl
 import datetime
+import pymysql
 
 firefox_options = Options()
 firefox_options.headless = True
-
-searchKeyWords = {
-    "www.datong-machine.com": [
-        "datong machinery"
-    ]
-}
 
 
 class SearchSpider(scrapy.Spider):
@@ -23,21 +18,37 @@ class SearchSpider(scrapy.Spider):
 
     def __init__(self, **kwargs):
         self.browser = webdriver.Firefox(options=firefox_options)
+        self.connect = pymysql.connect(
+            host='127.0.0.1',
+            port=3306,
+            db='scrapy_search',
+            user='root',
+            passwd='root',
+            charset='utf8',
+            use_unicode=True
+        )
+        self.cursor = self.connect.cursor()
         super().__init__(**kwargs)
 
     def start_requests(self):
-        # 构造start_urls
-        for domain in searchKeyWords:
-            for keyWord in searchKeyWords[domain]:
-                start_url = 'https://www.google.com/search?q=' + keyWord.replace(" ", "+") + '&domain=' + domain
-                self.start_urls.append(start_url)
+        # 从数据库获取要爬取keyword及domain
+        self.cursor.execute(
+            """
+            select keywords,domain from keywords
+            """
+        )
+        results = self.cursor.fetchall()
 
-        # 爬取start_urls
+        # 构造start_urls
+        for result in results:
+            start_url = 'https://www.google.com/search?q=' + result[0].replace(" ", "+") + '&domain=' + result[1]
+            self.start_urls.append(start_url)
+
+        # 爬取urls
         for url in self.start_urls:
             yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        print("爬取网址:" + response.url)
         query = dict(parse_qsl(urlparse(response.url).query))
         searchDomain = query['domain']
         searchKeyWord = query['q'].replace("+", " ")
@@ -48,7 +59,9 @@ class SearchSpider(scrapy.Spider):
         # 正常排序
         rank = 0
         count = 1
-        content_list = response.xpath("//div[@id='search']//div[@class='g']//div[@class='rc']//div[@class='r']/a//cite/text()").extract()
+        content_list = response.xpath(
+            "//div[@id='search']//div[@class='g']//div[@class='rc']//div[@class='r']/a//cite/text()"
+        ).extract()
         for content in content_list:
             if rank == 0 and str(content) == searchDomain:
                 rank = count
@@ -69,5 +82,6 @@ class SearchSpider(scrapy.Spider):
         item['updated_at'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         yield item
 
-    # def close(self, spider, reason):
-    #     # self.browser.quit()
+    def close(self, spider, reason):
+        self.browser.quit()
+        self.cursor.close()
